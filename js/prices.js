fdbe125ff55a929bdab988dbc01fe5af1538885a
dudha369 <duduha2010@gmail.com>
2024-05-02 00:01:45 +0300

    Update prices.js

--- js/prices.js
+++ js/prices.js
@@ -1,4 +1,4 @@
-const PRICES = {
+const PRICES= {
     // FORTNITE //
     "100VB": 8,
     "1000VB": 170,
@@ -8,16 +8,16 @@ const PRICES = {
     "PVE": 340,
     "Crew": 130,
     // DISCORD //
-    "Nitro Basic month": 70,
-    "Nitro Basic year": 700,
-    "Nitro month": 165,
-    "Nitro year": 1550,
+    "Nitro_Basic_month": 70,
+    "Nitro_Basic_year": 700,
+    "Nitro_month": 165,
+    "Nitro_year": 1550,
     // TELEGRAM //
-    "Telegram Premium month": 95,
-    "Telegram Premium year": 660,
+    "Telegram_Premium_month": 95,
+    "Telegram_Premium_year": 660,
     // SPOTIFY //
-    "Spotify Premium month": 100,
-    "Spotify Premium 3month": 265,
-    "Spotify Premium 6month": 520,
-    "Spotify Premium year": 900
+    "Spotify_Premium_month": 100,
+    "Spotify_Premium_3month": 265,
+    "Spotify_Premium_6month": 520,
+    "Spotify_Premium_year": 900
 }
