02a9de4c03a922c7a62b5446b8c8b3419f35646c
dudha369 <duduha2010@gmail.com>
2024-05-01 23:31:31 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-import PRICES from './prices.json' with { type: 'JSON' };
+const PRICES = {
+	// FORTNITE 
+	"100VB": 8
+	, "Crew": 130
+	, "1000VB": 170
+	, "2800VB": 425
+	, "5000VB": 715
+	, "13500VB": 1700
+	, "PVE": 340
+	, "Starter_Pack": 85,
+	// DISCORD 
+	"Nitro_Basic_month": 70
+	, "Nitro_Basic_year": 700
+	, "Nitro_month": 165
+	, "Nitro_year": 1550,
+	// TELEGRAM 
+	"Telegram_Premium_month": 95
+	, "Telegram_Premium_year": 660,
+	// SPOTIFY 
+	"Spotify_Premium_month": 100
+	, "Spotify_Premium_3month": 265
+	, "Spotify_Premium_6month": 520
+	, "Spotify_Premium_year": 900,
+	// TWITCH SUB
+};
+
 document.addEventListener("DOMContentLoaded", function () {
 	const prices = document.getElementsByClassName("price");
 	for (const item of prices) {
